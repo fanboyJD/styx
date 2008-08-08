f8a61403217a6e7aa8190eed272dfee3857ffb57
wusch <wusch@90e44d63-5046-0410-8e99-feec43159ead>
2008-08-08 15:14:56 +0000

    "  * Moved the Language parser to a new \"Fileparser\"-Class which makes it possible to put configuration to separate files "

---
+++ Source/Classes/Base/Fileparser.php
@@ -0,0 +1,55 @@
+<?php
+class Fileparser extends DynamicStorage {
+	
+	/* Big thanks to sorccu from the MooTools-Channel =) */
+	private $regex = array(
+			'/\s*([\w\.]+)\s*=\s*([\'\"])(.*?[^\\\]|)\2;/ism',
+			'/namespace\s+([\w\.]+)\s*\{((?:([\"\'])(?:.*?[^\\\\]|)\3|[^\}])*)\}/ism',
+		),
+		$replaces = array(
+			array("\'", "\""),
+			array("'", '"'),
+		);
+	
+	public function __construct($file){
+		$c = Cache::getInstance();
+		
+		$identifier = md5($file);
+		if(!Core::retrieve('debug')){
+			$array = $c->retrieve('Fileparser', $identifier);
+			if(is_array($array)){
+				$this->store($array);
+				return;
+			}
+		}
+		
+		$file = realpath(Core::retrieve('app.path').$file);
+		if(!$file || !file_exists($file))
+			return;
+		
+		$array = array();
+		$content = file_get_contents($file);
+		
+		preg_match_all($this->regex[1], $content, $m);
+		if(is_array($m[1]))
+			foreach($m[1] as $key => $val){
+				$content = str_replace($m[0][$key], '', $content);
+				
+				Hash::extend($array, $this->parse($m[2][$key], $val));
+			}
+		
+		$this->store($c->store('Fileparser', $identifier, Hash::extend($array, $this->parse($content)), ONE_DAY));
+	}
+	
+	private function parse($content, $prefix = null){
+		preg_match_all($this->regex[0], $content, $m);
+		
+		if(is_array($m[1]))
+			foreach($m[1] as $k => $v)
+				if($v && $m[3][$k])
+					$array[($prefix ? $prefix.'.' : '').$v] = str_replace($this->replaces[0], $this->replaces[1], $m[3][$k]);
+		
+		return pick($array, array());
+	}
+	
+}
--- Source/Classes/Base/Lang.php
+++ Source/Classes/Base/Lang.php
@@ -1,66 +1,30 @@
 <?php
-class Lang extends StaticStorage {
+class Lang {
 	
+	/**
+	 * @var Fileparser
+	 */
+	private static $Fileparser;
 	private static $lang = 'en';
 	
 	public static function setLanguage($lang){
 		self::$lang = strtolower(pick($lang, 'en'));
 		
-		self::readFile();
+		self::$Fileparser = new Fileparser('Language/'.self::$lang.'.lang');
 	}
 	
-	private static function readFile(){
-		$c = Cache::getInstance();
+	public static function retrieve($string){
+		if(!self::$Fileparser) return;
 		
-		if(!Core::retrieve('debug')){
-			$array = $c->retrieve('Lang', self::$lang);
-			if(is_array($array)){
-				self::store($array);
-				return;
-			}
-		}
-		
-		$file = realpath(Core::retrieve('app.path').'Language/'.self::$lang.'.lang');
-		if(!$file || !file_exists($file))
-			return;
-		
-		/* Big thanks to sorccu from the MooTools-Chan =) */
-		$regex = '/\s*([\w\.]+)\s*=\s*([\'\"])(.*?[^\\\]|)\2;/ism';
-		$str_rpl = array(
-			array("\'", "\{", "\}"),
-			array("'", "{", "}"),
-		);
-		
-		$content = file_get_contents($file);
-		
-		preg_match_all('/namespace\s+([\w\.]+)\s*\{((?:([\"\'])(?:.*?[^\\\\]|)\3|[^\}])*)\}/ism', $content, $m);
-		
-		if(is_array($m[1]))
-			foreach($m[1] as $key => $val){
-				$content = str_replace($m[0][$key], '', $content);
-				
-				preg_match_all($regex, $m[2][$key], $vars);
-				
-				if(is_array($vars[1]))
-					foreach($vars[1] as $k => $v)
-						if($v && $vars[3][$k])
-							$array[$val.'.'.$v] = str_replace($str_rpl[0], $str_rpl[1], $vars[3][$k]);
-			}
-		
-		preg_match_all($regex, $content, $m);
-		
-		if(is_array($m[1]))
-			foreach($m[1] as $k => $v)
-				if($v && $m[3][$k])
-					$array[$v] = str_replace($str_rpl[0], $str_rpl[1], $m[3][$k]);
-		
-		self::store($c->store('Lang', self::$lang, $array, ONE_DAY));
+		return self::$Fileparser->retrieve($string);
 	}
 	
 	public static function get(){
+		if(!self::$Fileparser) return;
+		
 		$args = func_get_args();
 		
-		$args[0] = self::retrieve($args[0]);
+		$args[0] = self::$Fileparser->retrieve($args[0]);
 		return call_user_func_array('sprintf', $args);
 	}
 	
