714e25a118ce67e7adcbf86c8ce6a9e69ed911dc
wusch <wusch@90e44d63-5046-0410-8e99-feec43159ead>
2009-02-06 13:44:15 +0000

    * Another little Sample update

--- Public/Sample/Templates/Layers/Page/menu.php
+++ Public/Sample/Templates/Layers/Page/menu.php
@@ -1,5 +1,6 @@
 <div id="menuleft"></div>
 <ul id="menu">
+<li><a href="<?php Response::link(); ?>">News</a></li>
 <?php
 foreach($this->Data as $data)
 	echo '<li><a href="'.$this->link($data['pagetitle']).'">'.$data['title'].'</a></li>';
